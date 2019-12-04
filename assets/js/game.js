80fcc8ee2127f9e99503463ab06f6e72afc42a93
Nicole Marty <nmarty310@gmail.com>
2019-12-04 16:58:12 -0600

    logging all characters as own objects

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,47 +1,66 @@
 // Initialize game
 $(document).ready(function() {
-    var charObj = {
-        "Go-Go": {
+
+    var charObj = [{
+            //charObj[0]
             name: "Go-Go",
             health: 120,
             attack: 8,
             imageUrl: "assets/images/go-go-crazy.jpg",
             enemyAttackBack: 15
         },
-        "Purge": {
+        {
+            //charObj[1]
             name: "Purge",
             health: 100,
             attack: 14,
             imageUrl: "assets/images/purge-dude.jpg",
             enemyAttackBack: 5
         },
-        "Blue Ice": {
+        {
+            //charObj[2]
             name: "Blue Ice",
             health: 150,
             attack: 8,
             imageUrl: "assets/images/blue-ice.jpg",
             enemyAttackBack: 5
         },
-        "Pantera": {
+        {
+            //charObj[3]
             name: "Pantera",
             health: 180,
             attack: 7,
-            imageUrl: "assets/images/robo-panther.jpg",
+            imageUrl: "assets/images/pantera.jpg",
             enemyAttackBack: 25
         }
 
-    };
-    for (var key in charObj) {
-        console.log("key: " + key + "\n" + "value: " + charObj[key]);
+    ];
+
+    for (var i = 0; i < charObj.length; i++) {
+
     }
-    console.log(charObj);
+    console.log(charObj[0].name);
+    //returns "Go-Go"
     var charDiv = $("#character_choices");
+    charDiv.append(charObj[0]);
+
 
     $.each(charObj, function(key, character) {
-        charDiv.append("<div>" + "Name: " + character.name + "<div>");
-        charDiv.append("<div>" + "Health: " + character.health + "<div>");
-        charDiv.append("<div>" + "Attack: " + character.attack + "<div>");
-        charDiv.append("<div>" + "imageUrl: " + character.imageUrl + "<div>");
-        charDiv.append("<div>" + "Enemy Attack Back: " + character.enemyAttackBack + "<div>");
-    })
+        charDiv.append("<div class='profpic'>" + character.imageUrl + "</div>" + "<div class='character'>" + character.name + "</div>" + "<div class='health'>" + character.health + "</div>" + "<div class='attack'>" + character.attack + "</div>" + "<div class='enemy-attack-back'>" + character.enemyAttackBack + "</div>");
+        console.log(charObj);
+    });
+
+    var gogo = charObj[0];
+    console.log(gogo);
+    var purge = charObj[1];
+    console.log(purge);
+    var blueice = charObj[2];
+    console.log(blueice);
+    var pantera = charObj[3];
+    console.log(pantera);
+
+    var characters = [gogo, purge, blueice, pantera];
+    console.log(characters);
+
+
 });
